78db1a009b12e86738c59b6c208adb5783eee6ad
fibarrafdec <139788771+fibarrafdec@users.noreply.github.com>
2023-09-06 17:39:38 -0300

    Create script.js
    Date/Time display

---
+++ script.js
@@ -0,0 +1,9 @@
+let timeDisplayEl = $('#time-display');
+
+function displayTime() {
+    let rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+}  
+
+displayTime();
+setInterval(displayTime, 1000);
